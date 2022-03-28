233a22f9ef49dcfdae1172f591ccae834d77546b
selam123 <beletelakachew@gmail.com>
2022-03-28 01:23:52 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+understand Even more pointers, arrays and strings exericise
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *);
+char *leet(char *);
+int _strlen(char *s);
+int match_found(char *h, char *n);
+int _islower(int c);
+int _isNewWord(int a);
+char *rot13(char *s);
+void print_number(int n);
+int digit_len(int n);
+int myPow(int a);
+
+#endif /* MAIN_H */
