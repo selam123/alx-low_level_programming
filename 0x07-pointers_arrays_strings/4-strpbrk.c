f7bc1233c0465b0370439b5321cce774a4db3bde
selam123 <beletelakachew@gmail.com>
2022-03-28 04:18:33 -0700

    task4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurrence in a string of any bytes
+ * in a reference string
+ * @s: pointer to the string to be searched
+ * @accept: pointer to the reference string
+ * Return: pointer s at address of first match or NULL.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int idxs, idxa;
+
+	idxs = 0;
+	while (s[idxs] != '\0')
+	{
+		idxa = 0;
+		while (accept[idxa] != '\0')
+		{
+			if (s[idxs] == accept[idxa])
+				return (s + idxs);
+			idxa++;
+		}
+		idxs++;
+	}
+	return ('\0');
+
